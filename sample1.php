35b6a90ca12c3b4b5a4bb56b926db5b3222bc9a8
vaiha1 <mnkumar.vaiha@gmail.com>
2014-12-02 10:30:25 +0530

    ii

--- sample1.php
+++ sample1.php
@@ -1,25 +1,15 @@
 <?php
-$dbhost = 'tagya-db.cgxgvzgqoani.us-east-1.rds.amazonaws.com';
+/*$dbhost = 'tagya-db.cgxgvzgqoani.us-east-1.rds.amazonaws.com';
 $username = 'tagya_user_1';
 $password = 'S8DEh=)xz7wYT#-';
 $dbname = 'tagya_db';
 
 $link = mysql_connect($dbhost, $username, $password);
 mysql_select_db($dbname);
-?>
-<html>
-<body>
 
-<form action="sample1.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="UploadImage" name="submit">
-</form>
+*/
 
-</body>
-</html>
 
-<?php
 $bucket = 'tagyas3';
 $keyname = 'fileToUpload';
 // $filepath should be absolute path to a file on disk						
