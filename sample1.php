9224727aa5bd0f0fe8162f8fb64837b103862b2e
vaiha1 <mnkumar.vaiha@gmail.com>
2014-12-02 10:02:28 +0530

    image upload

--- sample1.php
+++ sample1.php
@@ -10,7 +10,7 @@ mysql_select_db($dbname);
 <html>
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="sample1.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="UploadImage" name="submit">
