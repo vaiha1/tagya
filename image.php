faf2bdc1c4bf645232bf4e5047a80f0d3d892fda
vaiha1 <mnkumar.vaiha@gmail.com>
2014-12-02 10:31:07 +0530

    im

---
+++ image.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="sample1.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="UploadImage" name="submit">
+</form>
+
+</body>
+</html>
