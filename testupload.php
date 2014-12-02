d35bcb1f688abc1990e4d75360ffb1e08ae20365
sriramvaiha <shriramvaiha@gmail.com>
2014-12-02 13:33:35 +0530

    up

--- testupload.php
+++ testupload.php
@@ -1,14 +1,14 @@
 <?php
 echo 'hai';
 require('aws/aws-autoloader.php');
-echo "require included";
+echo "<br>require included";
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 /* $s3 =  S3Client::factory(array(
     'key'    => 'AKIAJ6ZDK6VP7WZUL4RQ',
     'secret' => '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',
 )); */
 $s3 =  S3Client::factory();
-
+echo "<br>s3 client connected.";
 $bucket = 'tagyas3';
 echo $bucket;
 ?>
