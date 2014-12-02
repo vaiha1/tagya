c6423b189040f64558085b331cfab9849fb5ae3c
vaiha1 <mnkumar.vaiha@gmail.com>
2014-12-02 11:20:16 +0530

    i

--- sample_upload.php
+++ sample_upload.php
@@ -1,6 +1,7 @@
 <?php
 //echo 'hai';
 require('vendor/autoload.php');
+echo "hai";
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = Aws\S3\S3Client::factory();
 echo $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
